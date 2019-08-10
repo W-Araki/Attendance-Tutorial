59f7c5cb222b16db54007669b3a4e7a4173943c7
Araki <aino18nichidai18@gmail.com>
2019-08-10 12:01:15 +0000

    Attendance8-5-1fin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -12,11 +18,23 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "新規作成に成功しました！"
+      flash[:success] = '新規作成に成功しました！'
       redirect_to @user
       # 保存に成功した場合は、ここに記述した処理が実行されます。
     else
       render :new
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      render :edit  
     end
   end
 
@@ -24,5 +42,21 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def set_user
+      @user = User.find(params[:id])
+    end
+    
+    def logged_in_user
+      unless logged_in?
+      store_location
+      flash[:danger] = "ログインしてください。"
+      redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
