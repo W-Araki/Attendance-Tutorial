cb6c838dde92247e2f6de08fee38c6f5cc74f420
Araki <aino18nichidai18@gmail.com>
2019-08-26 14:05:39 +0000

    Finish add-user-attendances

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,44 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-  
+
   $days_of_the_week = %w{日 月 火 水 木 金 土}
+
+  # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
+  def set_one_month 
+  @first_day = params[:date].nil? ?
+  Date.current.beginning_of_month : params[:date].to_date
+  @last_day = @first_day.end_of_month
+  one_month = [*@first_day..@last_day]
+
+  @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
+
+  unless one_month.count == @attendances.count
+    ActiveRecord::Base.transaction do
+      one_month.each { |day| @user.attendances.create!(worked_on: day) }
+    end
+    @attendances = @user.attendances.where(worked_on: @first_day..@last_day).order(:worked_on)
+  end
+
+rescue ActiveRecord::RecordInvalid
+  flash[:danger] = "ページ情報の取得に失敗しました、再アクセスしてください。"
+  redirect_to root_url
+  end
 end
- 
+
+class AttendancesController < ApplicationController
+
+  def update
+    @user = User.find(params[:user_id])
+    @attendance = Attendance.find(params[:id])
+    # 出勤時間が未登録であることを判定します。
+    if @attendance.started_at.nil?
+      if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
+        flash[:info] = "おはようございます！"
+      else
+        flash[:danger] = "勤怠登録に失敗しました。やり直してください。"
+      end
+    end
+    redirect_to @user
+  end
+end
